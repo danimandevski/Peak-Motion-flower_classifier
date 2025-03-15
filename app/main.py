390b3e1afe2a7a190149246f5a673d1b21e3fbfb
danimandevski <danimandevski@gmail.com>
2025-03-15 22:55:01 +0200

    Configure CORS Middleware remove redundant files

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from services.planetnet_classification import classify_plant
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8100",
+    "http://127.0.0.1:8100",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/classify")
 async def classify_plant_endpoint(image: UploadFile = File(...)):
@@ -11,7 +25,7 @@ async def classify_plant_endpoint(image: UploadFile = File(...)):
 
     best_match = classify_plant(image_bytes, image.filename, image.content_type)
 
-    return {best_match}
+    return {"best_match": best_match}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
